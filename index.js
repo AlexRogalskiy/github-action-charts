4a77c045b928129bb204d93d58e5998e71a6dc7c
ADMSK\AVROGAL1 <avrogal1@mts.ru>
2021-02-12 17:34:44 +0300

    docs: updates on workflows
    Added information on documentation

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const webshot = require('webshot-node');
 const path = require('path');
 const config = require('./src/config');
 
-async function snap(url, file, options) {
+async function createSnapshot(url, file, options) {
+  console.log(`Generating file with name ${file} by ${url} with ${options}`);
+
   try {
     await webshot(url, file, options, err => {
       if (err) throw err;
@@ -16,6 +18,13 @@ async function snap(url, file, options) {
   }
 }
 
+function createOptions(width, height) {
+  return {
+    shotSize: { width, height },
+    windowSize: { width, height },
+  };
+}
+
 async function run() {
   const fileName = core.getInput('name') || config.name;
   const filePath = core.getInput('path') || config.path;
@@ -24,30 +33,11 @@ async function run() {
   const width = core.getInput('width');
   const height = core.getInput('height');
 
-  const sourceOptions = {
-    shotSize: { width, height },
-    windowSize: { width, height },
-  };
-
   const url = `https://styled-charts.vercel.app/api?url=${source}&width=${width}&height=${height}`;
-  // const file = path.join(filePath, `${fileName}.png`);
-  const file = 'demo.png';
-  // const options = { ...config.options, ...sourceOptions };
+  const file = path.join(filePath, `${fileName}.png`);
+  const options = { ...config.options, ...createOptions(width, height) };
 
-  const options = {
-    shotSize: {
-      width: 1024,
-      height: 512,
-    },
-    windowSize: {
-      width: 1024,
-      height: 768,
-    },
-  };
-
-  console.log(options);
-
-  await snap(url, file, options);
+  await createSnapshot(url, file, options);
 
   core.setOutput('image', 'image downloaded in root directory');
 }
